330fcca6aca0524a18fe60a4321c8e8739b79052
Vaana <35535603+vana123@users.noreply.github.com>
2022-07-05 16:04:18 +0300

    fairBaze setUp

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { setupStore } from "./store/store";
+import "./fireBaze";
 
 const store = setupStore();
 
