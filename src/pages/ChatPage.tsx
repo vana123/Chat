6e7572f6da401766d674e94cfb8b23febafdae31
Vaana <35535603+vana123@users.noreply.github.com>
2022-07-07 00:50:47 +0300

    initial firestore

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { getAuth } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Navigate } from "react-router-dom";
 import { Container } from "@mui/system";
 import { Button, Grid, TextField } from "@mui/material";
+import { db } from "./../fireBase";
 
 export const ChatPage = () => {
 	const auth = getAuth();
 	const [user] = useAuthState(auth);
+
+	const [inputMassage, setInputMassage] = useState("");
+	const onSend = async () => {
+		console.log("hi");
+	};
+
 	if (!!user) {
 		return (
 			<Container maxWidth="md">
@@ -23,7 +30,9 @@ export const ChatPage = () => {
 							border: "1px solid gray",
 							overflowY: "auto",
 						}}
-					></div>
+					>
+						{JSON.stringify(db)}
+					</div>
 					<Grid
 						container
 						item
@@ -34,10 +43,14 @@ export const ChatPage = () => {
 						<TextField
 							fullWidth
 							variant={"outlined"}
-
-							// onChange={(e) => setValue(e.target.value)}
+							value={inputMassage}
+							onChange={(
+								e: React.ChangeEvent<HTMLInputElement>
+							) => setInputMassage(e.target.value)}
 						/>
-						<Button variant={"outlined"}>Отправить</Button>
+						<Button variant={"outlined"} onClick={onSend}>
+							Send
+						</Button>
 					</Grid>
 				</Grid>
 			</Container>
